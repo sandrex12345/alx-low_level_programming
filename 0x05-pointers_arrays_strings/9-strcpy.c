641f1aeefc1a90a849a99664918d16aa839a4f1d
sandrex12345 <osaraose1@gmail.com>
2023-09-18 16:54:49 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointedto by src
+ * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
