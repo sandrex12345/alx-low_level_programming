d313b632541a7db8d846bf8aff901d69f7414f26
sandrex12345 <osaraose1@gmail.com>
2023-09-20 06:49:45 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,27 @@
 #include "main.h"
-/**
- * _strcmp - conmpare string values
- * @s1: input value
- * @s2: input value
- * Return: s1[i] - s2[i]
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		dest[j] = src[j];
+		j++;
 	}
-	return (0);
+	while (j < n)
+	{
+		dest[j] = '\0'
+		j++;
+	}
+	return (dest);
+}
 }
